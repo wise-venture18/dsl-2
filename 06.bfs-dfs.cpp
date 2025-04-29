90170ddb222c0a5a923467fdae475c7c230355d2
wise-venture18 <wiseventure.business@gmail.com>
2025-04-29 23:12:21 +0530

    Update 06.bfs-dfs.cpp

--- 06.bfs-dfs.cpp
+++ 06.bfs-dfs.cpp
@@ -1,93 +1,123 @@
-// Sample Input :
-// Enter number of vertices: 5
+// 🧾 Sample Input:
+// Enter number of vertices (max 10): 5
 // Enter number of edges: 6
 
-// EDGES:
-// Enter edge 0 (in the format: vertex1 vertex2): 0 1
-// Enter edge 1 (in the format: vertex1 vertex2): 0 2
+// EDGES (vertex1 vertex2 format):
+// Edge 0: 0 1
+// Edge 1: 0 2
+// Edge 2: 1 3
+// Edge 3: 2 3
+// Edge 4: 3 4
+// Edge 5: 1 4
 
-#include <iostream> 
-#include <stdlib.h> 
-using namespace std; 
- 
-int cost[10][10], i, j, k, n, m, qu[10], front = 0, rear = 0, v, visited[10], visit[10]; 
-int stk[10], top = 0, visit1[10], visited1[10]; 
- 
-int main() 
-{ 
-    cout << "Enter number of vertices: "; 
-    cin >> n; 
-    cout << "Enter number of edges: "; 
-    cin >> m; 
- 
-    // Initialize adjacency matrix 
-    for (i = 0; i < n; i++) { 
-        for (j = 0; j < n; j++) { 
-            cost[i][j] = 0; 
-        } 
-    } 
- 
-    // Read edges 
-    cout << "\nEDGES:\n"; 
-    for (k = 0; k <= m - 1; k++) { 
-        cout << "Enter edge " << k << " (in the format: vertex1 vertex2): "; 
-        cin >> i >> j; 
-        cost[i][j] = 1; 
-        cost[j][i] = 1;  // Because the graph is undirected 
-    } 
- 
-    // Display adjacency matrix 
-    cout << "The adjacency matrix of the graph is:\n"; 
-    for (i = 0; i < n; i++) { 
-        for (j = 0; j < n; j++) { 
-            cout << " " << cost[i][j]; 
-        } 
-        cout << endl; 
-    } 
- 
-    // BFS 
-    cout << "Enter initial vertex for BFS: "; 
-    cin >> v; 
-    cout << "The BFS of the Graph is: \n"; 
-    cout << v << " "; 
-    visited[v] = 1; 
-    k = 0; 
-    while (k < n) { 
-        for (j = 0; j < n; j++) { 
-            if (cost[v][j] != 0 && visited[j] != 1 && visit[j] != 1) { 
-                visit[j] = 1; 
-                qu[rear++] = j; 
-            } 
-        } 
-        v = qu[front++]; 
-        cout << v << " "; 
-        k++; 
-        visit[v] = 0; 
-        visited[v] = 1; 
-    } 
-    cout << endl; 
- 
-    // DFS 
-    cout << "Enter initial vertex for DFS: "; 
-    cin >> v; 
-    cout << "The DFS of the Graph is: \n"; 
-    cout << v << " "; 
-    visited1[v] = 1; 
-    k = 0; 
-    stk[top++] = v; 
-    while (k < n) { 
-        for (j = n - 1; j >= 0; j--) { 
-            if (cost[v][j] != 0 && visited1[j] != 1 && visit1[j] != 1) { 
-                visit1[j] = 1; 
-                stk[top++] = j; 
-            } 
-        } 
-        v = stk[--top]; 
-        cout << v << " "; 
-        k++; 
-        visit1[v] = 0; 
-        visited1[v] = 1; 
-    } 
-    cout << endl; 
-    return 0; 
-} 
+// Enter starting vertex (0 to 4) for BFS: 0
+// Enter starting vertex (0 to 4) for DFS: 0
+//___________________________________________
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <cstring>
+using namespace std;
+
+const int MAX = 10;
+
+// Graph representations
+int adjMatrix[MAX][MAX];            // For DFS
+int adjList[MAX][MAX];             // For BFS
+int listSize[MAX];                 // To track size of each row in adjList
+
+// Sample landmark names
+char landmarks[MAX][20] = {
+    "Library", "Canteen", "Lab", "Hostel", "Main Gate"
+};
+
+int n, m;
+
+// DFS using adjacency matrix
+void DFS(int start) {
+    int visited[MAX] = {0};
+    stack<int> s;
+
+    s.push(start);
+    visited[start] = 1;
+
+    cout << "DFS traversal: ";
+    while (!s.empty()) {
+        int v = s.top();
+        s.pop();
+        cout << landmarks[v] << " ";
+
+        // Traverse neighbors
+        for (int i = n - 1; i >= 0; i--) {
+            if (adjMatrix[v][i] && !visited[i]) {
+                s.push(i);
+                visited[i] = 1;
+            }
+        }
+    }
+    cout << endl;
+}
+
+// BFS using adjacency list
+void BFS(int start) {
+    int visited[MAX] = {0};
+    queue<int> q;
+
+    q.push(start);
+    visited[start] = 1;
+
+    cout << "BFS traversal: ";
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop();
+        cout << landmarks[v] << " ";
+
+        for (int i = 0; i < listSize[v]; i++) {
+            int neighbor = adjList[v][i];
+            if (!visited[neighbor]) {
+                q.push(neighbor);
+                visited[neighbor] = 1;
+            }
+        }
+    }
+    cout << endl;
+}
+
+int main() {
+    cout << "Enter number of vertices (max 10): ";
+    cin >> n;
+
+    cout << "Enter number of edges: ";
+    cin >> m;
+
+    // Initialize
+    memset(adjMatrix, 0, sizeof(adjMatrix));
+    memset(adjList, 0, sizeof(adjList));
+    memset(listSize, 0, sizeof(listSize));
+
+    cout << "\nEDGES (vertex1 vertex2 format):\n";
+    for (int k = 0; k < m; k++) {
+        int u, v;
+        cout << "Edge " << k << ": ";
+        cin >> u >> v;
+
+        // Adjacency matrix (for DFS)
+        adjMatrix[u][v] = 1;
+        adjMatrix[v][u] = 1;
+
+        // Adjacency list (for BFS)
+        adjList[u][listSize[u]++] = v;
+        adjList[v][listSize[v]++] = u;
+    }
+
+    int start;
+    cout << "\nEnter starting vertex (0 to " << n - 1 << ") for BFS: ";
+    cin >> start;
+    BFS(start);
+
+    cout << "\nEnter starting vertex (0 to " << n - 1 << ") for DFS: ";
+    cin >> start;
+    DFS(start);
+
+    return 0;
+}
